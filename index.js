9c97d2f3eb957d89bc4e032ffde1303234e10fc7
Sindre Sorhus <sindresorhus@gmail.com>
2019-02-15 16:30:17 +0700

    Add `function-calc-no-invalid` rule
    https://stylelint.io/user-guide/rules/function-calc-no-invalid/

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ module.exports = {
 		'color-no-invalid-hex': true,
 		'font-family-no-duplicate-names': true,
 		'font-family-no-missing-generic-family-keyword': true,
+		'function-calc-no-invalid': true,
 		'function-calc-no-unspaced-operator': true,
 		'function-linear-gradient-no-nonstandard-direction': true,
 		'string-no-newline': true,
