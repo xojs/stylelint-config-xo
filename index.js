b8a5a3763c5515ea2db66ea80ab7d71474947a36
Scott Busche <swbusche@gmail.com>
2021-10-24 13:46:22 -0500

    Remove option removed in Stylelint 14

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ module.exports = {
 		'color-no-invalid-hex': true,
 		'font-family-no-duplicate-names': true,
 		'font-family-no-missing-generic-family-keyword': true,
-		'function-calc-no-invalid': true,
 		'function-calc-no-unspaced-operator': true,
 		'function-linear-gradient-no-nonstandard-direction': true,
 		'string-no-newline': true,
