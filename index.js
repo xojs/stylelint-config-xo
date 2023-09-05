196458f637c8d53052ca3b19f67954a1fc7886b1
Federico Brigante <me@fregante.com>
2023-09-05 11:48:07 +0800

    Drop stylistic rules

--- index.js
+++ index.js
@@ -69,7 +69,6 @@ module.exports = {
 		'no-duplicate-at-import-rules': true,
 		'no-duplicate-selectors': true,
 		'no-empty-source': true,
-		'no-extra-semicolons': true,
 		'no-invalid-double-slash-comments': true,
 		'alpha-value-notation': 'percentage',
 		'hue-degree-notation': 'angle',
@@ -116,7 +115,6 @@ module.exports = {
 		'selector-max-attribute': 8,
 		'selector-max-class': 8,
 		'selector-max-compound-selectors': 8,
-		'selector-max-empty-lines': 0,
 		'selector-max-universal': 1,
 		'selector-no-vendor-prefix': [
 			true,
@@ -157,68 +155,17 @@ module.exports = {
 				]
 			}
 		],
-		'color-hex-case': 'lower',
 		'color-hex-length': 'short',
 		'font-family-name-quotes': 'always-where-recommended',
 		'font-weight-notation': 'named-where-possible',
-		'function-comma-newline-after': 'always-multi-line',
-		'function-comma-newline-before': 'never-multi-line',
-		'function-comma-space-after': 'always-single-line',
-		'function-max-empty-lines': 0,
 		'function-name-case': 'lower',
-		'function-parentheses-newline-inside': 'always-multi-line',
-		'function-parentheses-space-inside': 'never-single-line',
 		'function-url-quotes': 'always',
-		'function-whitespace-after': 'always',
-		'number-leading-zero': 'always',
-		'number-no-trailing-zeros': true,
-		'string-quotes': [
-			'single',
-			{
-				avoidEscape: false
-			}
-		],
 		'length-zero-no-unit': true,
-		'unit-case': 'lower',
 		'value-keyword-case': 'lower',
-		'value-list-comma-newline-after': 'always-multi-line',
-		'value-list-comma-newline-before': 'never-multi-line',
-		'value-list-comma-space-after': 'always-single-line',
-		'value-list-comma-space-before': 'never',
-		'value-list-max-empty-lines': 0,
-		'property-case': 'lower',
-		'declaration-bang-space-after': 'never',
-		'declaration-bang-space-before': 'always',
-		'declaration-colon-newline-after': 'always-multi-line',
-		'declaration-colon-space-after': 'always-single-line',
-		'declaration-colon-space-before': 'never',
 		'declaration-empty-line-before': 'never',
-		'declaration-block-semicolon-newline-after': 'always',
-		'declaration-block-semicolon-newline-before': 'never-multi-line',
-		'declaration-block-semicolon-space-after': 'always-single-line',
-		'declaration-block-semicolon-space-before': 'never',
-		'declaration-block-trailing-semicolon': 'always',
-		'block-closing-brace-empty-line-before': 'never',
-		'block-closing-brace-newline-after': 'always',
-		'block-closing-brace-newline-before': 'always',
-		'block-opening-brace-newline-after': 'always',
-		'block-opening-brace-space-before': 'always',
-		'selector-attribute-brackets-space-inside': 'never',
-		'selector-attribute-operator-space-after': 'never',
-		'selector-attribute-operator-space-before': 'never',
 		'selector-attribute-quotes': 'always',
-		'selector-combinator-space-after': 'always',
-		'selector-combinator-space-before': 'always',
-		'selector-descendant-combinator-no-non-space': true,
-		'selector-pseudo-class-case': 'lower',
-		'selector-pseudo-class-parentheses-space-inside': 'never',
-		'selector-pseudo-element-case': 'lower',
 		'selector-pseudo-element-colon-notation': 'double',
 		'selector-type-case': 'lower',
-		'selector-list-comma-newline-after': 'always',
-		'selector-list-comma-newline-before': 'never-multi-line',
-		'selector-list-comma-space-after': 'always-single-line',
-		'selector-list-comma-space-before': 'never',
 		'rule-empty-line-before': [
 			'always',
 			{
@@ -231,15 +178,6 @@ module.exports = {
 				]
 			}
 		],
-		'media-feature-colon-space-after': 'always',
-		'media-feature-colon-space-before': 'never',
-		'media-feature-name-case': 'lower',
-		'media-feature-parentheses-space-inside': 'never',
-		'media-feature-range-operator-space-after': 'always',
-		'media-feature-range-operator-space-before': 'always',
-		'media-query-list-comma-newline-after': 'always',
-		'media-query-list-comma-newline-before': 'never-multi-line',
-		'media-query-list-comma-space-before': 'never',
 		'at-rule-empty-line-before': [
 			'always',
 			{
@@ -252,24 +190,7 @@ module.exports = {
 				]
 			}
 		],
-		'at-rule-name-case': 'lower',
-		'at-rule-name-newline-after': 'always-multi-line',
-		'at-rule-name-space-after': 'always-single-line',
-		'at-rule-semicolon-newline-after': 'always',
-		'at-rule-semicolon-space-before': 'never',
 		'comment-whitespace-inside': 'always',
-		indentation: [
-			'tab',
-			{
-				baseIndentLevel: 1
-			}
-		],
-		linebreaks: 'unix',
-		'max-empty-lines': 2,
-		'no-eol-whitespace': true,
-		'no-missing-end-of-source-newline': true,
-		'no-empty-first-line': true,
-		'unicode-bom': 'never',
 
 		// `stylelint-order`
 		'order/order': [
